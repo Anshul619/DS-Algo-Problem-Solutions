6cceb3183a53254516339d0c28cc5166abffb7f4
Anshul Agrawal <anshul.agraw999@gmail.com>
2022-04-26 12:55:17 +0530

    145. Binary Tree Postorder Traversal

---
+++ src/BinaryTreePostOrderTraversalIteration145.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+import java.util.*;
+
+class BinaryTreePostOrderTraversalIteration145 {
+    
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> output = new ArrayList<Integer>();
+        
+        return output;
+    }
+}
---
+++ src/BinaryTreePostOrderTraversalRecursion145.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+import java.util.*;
+
+class BinaryTreePostOrderTraversalRecursion145 {
+    void recursion(TreeNode node, List<Integer> output) {
+        
+        if (node == null) {
+            return;
+        }
+        
+        if (node.left != null) {
+            recursion(node.left, output);
+        }
+        
+        if (node.right != null) {
+            recursion(node.right, output);
+        }    
+        
+        output.add(node.val);
+    }
+    
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> output = new ArrayList<Integer>();
+        
+        recursion(root, output);
+        
+        return output;
+    }
+}
