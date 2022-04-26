cf2135321dc5ef2b6fcab57188df8931d94a1bc4
Anshul Agrawal <anshul.agraw999@gmail.com>
2022-04-26 18:25:47 +0530

    145. Binary Tree Postorder Traversal

--- src/leetCodeProblems/BinaryTreePostOrderTraversalIteration145.java
+++ src/leetCodeProblems/BinaryTreePostOrderTraversalIteration145.java
@@ -6,6 +6,10 @@ package leetCodeProblems;/*
  */
 
 /**
+ * LeetCode Problem Link - https://leetcode.com/problems/binary-tree-postorder-traversal/
+ * */
+
+/**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
@@ -30,4 +34,22 @@ class BinaryTreePostOrderTraversalIteration145 {
         
         return output;
     }
+    
+    static class TreeNode {
+    	int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+    }
 }
+
--- src/leetCodeProblems/BinaryTreePostOrderTraversalRecursion145.java
+++ src/leetCodeProblems/BinaryTreePostOrderTraversalRecursion145.java
@@ -1,31 +1,15 @@
-package leetCodeProblems;/*
-    Problem -
-    Solution -
-    Time Complexity -
-    Space Complexity -
- */
+package leetCodeProblems;
 
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+ * LeetCode Problem Link - https://leetcode.com/problems/binary-tree-postorder-traversal/
+ * */
 
-import java.util.*;
-
-class BinaryTreePostOrderTraversalRecursion145 {
-    void recursion(TreeNode node, List<Integer> output) {
+public class BinaryTreePostOrderTraversalRecursion145 {
+	
+	void recursion(TreeNode node, List<Integer> output) {
         
         if (node == null) {
             return;
@@ -49,4 +33,39 @@ class BinaryTreePostOrderTraversalRecursion145 {
         
         return output;
     }
+    
+    public static void main(String[] args) {
+    	
+    	TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+        
+        BinaryTreePostOrderTraversalRecursion145 ob = new BinaryTreePostOrderTraversalRecursion145();
+        
+        List<Integer> output = ob.postorderTraversal(root);
+        
+        System.out.println(output);
+    	
+    }
+    
+    static class TreeNode {
+    	int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
 }
