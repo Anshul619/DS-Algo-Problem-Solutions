b3bcdc6fde9d65bc53ce69758ef3c069f68fe31d
Anshul Agrawal <anshul.agraw999@gmail.com>
2022-04-27 17:16:38 +0530

    Binary Search

---
+++ src/leetCodeProblems/BinarySearch/BinarySearch704.java
@@ -0,0 +1,41 @@
+package leetCodeProblems.BinarySearch;
+
+/**
+ * Leet Code - https://leetcode.com/problems/binary-search/submissions/
+ *
+ * @author anshul.agrawal
+ *
+ */
+public class BinarySearch704 {
+	public static int binarySearch(int start, int end, int[] nums1, int target) {
+        
+        int middle = (start+end)/2;
+            
+        if (start < 0 || end < 0) {
+            return -1;
+        }
+        
+        if (nums1[middle] == target) {
+            return middle;
+        }
+        else if (start == end) {
+            return -1;
+        }
+        else if (nums1[middle] > target) {
+            end = middle - 1; 
+        }
+        else {
+            start = middle + 1;
+        }
+        
+        return binarySearch(start, end, nums1, target);
+    }
+    
+    public int search(int[] nums, int target) {
+        
+        int searchElement = binarySearch(0, nums.length-1, nums, target);
+        
+        return searchElement;
+        
+    }
+}
---
+++ src/leetCodeProblems/BinarySearch/BinarySearchRotatedSortedArray.java
@@ -0,0 +1,57 @@
+package leetCodeProblems.BinarySearch;
+
+/**
+ * Leet Code - https://leetcode.com/problems/binary-search/submissions/
+ *
+ * @author anshul.agrawal
+ *
+ */
+
+public class BinarySearchRotatedSortedArray {
+	
+	public static int binarySearch(int start, int end, int[] nums1, int target) {
+        
+        int middle = (start+end)/2;
+         
+        if (start < 0 || end < 0 || start > end) {
+            return -1;
+        }
+        
+        if (nums1[middle] == target) {
+            return middle;
+        }
+        else if (start == end) {
+            return -1;
+        }
+        else if (nums1[start] <= nums1[middle]) { // Left side is sorted array
+            
+        	 // Target element value is b/w start and middle
+            if (nums1[start] <= target && target <= nums1[middle]) {
+                end = middle - 1;
+            }
+            else {
+                start = middle + 1;
+            }
+            
+        }
+        else {  // right side is SORTED
+
+        	if (nums1[middle] <= target && target <= nums1[end]) {
+        		start = middle + 1;
+        	}
+        	else {
+        		end = middle - 1;
+        	}
+        }
+        
+        return binarySearch(start, end, nums1, target);
+    }
+    
+    public int search(int[] nums, int target) {
+        
+        int searchElement = binarySearch(0, nums.length-1, nums, target);
+        
+        return searchElement;
+    }
+
+}
