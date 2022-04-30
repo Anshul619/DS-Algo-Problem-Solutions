aec73fd961601be8941ded5444a3981c80d28464
Anshul Agrawal <anshul.agraw999@gmail.com>
2022-04-30 10:14:23 +0530

    16. 3 Sum Closest

---
+++ src/leetCodeProblems/TwoPointers/ThreeSumClosest16.java
@@ -0,0 +1,52 @@
+package leetCodeProblems.TwoPointers;
+
+public class ThreeSumClosest16 {
+	public int threeSumClosest(int[] nums, int target) {
+        
+        int targetSum = target;
+        int closestSum;
+
+        if (nums.length > 2) {
+            closestSum = nums[0] + nums[1] + nums[nums.length-1];
+        }
+        else {
+            closestSum = -1;
+        }
+
+        Arrays.sort(nums);
+    
+        for(int fixedPointer=0; fixedPointer < nums.length; fixedPointer++) {
+            
+            int leftPointer = fixedPointer+1;
+            int rightPointer = nums.length-1;
+            
+            while(leftPointer < rightPointer) {
+                
+                int sum = nums[fixedPointer] + nums[leftPointer] + nums[rightPointer];
+                int sumDiff = sum - targetSum;
+                int closestDiff = closestSum - targetSum;
+
+                if (sum == targetSum) {
+                    return targetSum;
+                }
+                else if(sum < targetSum) {
+                    leftPointer++;
+                }
+                else {
+                    rightPointer--;
+                }
+
+                //System.out.println("Sum diff -> " + Math.abs(sumDiff));
+                //System.out.println("Closest diff -> " + Math.abs(closestDiff));
+                
+                if (Math.abs(sumDiff) < Math.abs(closestDiff)) {
+                    closestSum = sum;
+                }
+                
+            }
+        }
+        
+        return closestSum;
+        
+    }
+}
