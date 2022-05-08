fef9bf8a72cae87e21c03522329eb90cceddb1e2
Anshul Agrawal <anshul.agraw999@gmail.com>
2022-05-08 13:43:09 +0530

    20. Valid Parentheses

---
+++ src/leetCodeProblems/StacksAndQueues/ValidParentheses20.java
@@ -0,0 +1,107 @@
+package leetCodeProblems.StacksAndQueues;
+
+/**
+ * LeetCode Link - https://leetcode.com/problems/valid-parentheses/
+ * InterviewBit Link - https://www.interviewbit.com/problems/balanced-parantheses/
+ * 
+ * @author anshul.agrawal
+ *
+ */
+public class ValidParentheses20 {
+	
+	static class ParenthesesStack {
+		private int maxSize;
+		private String[] stackArray;
+		private int top;
+
+		public ParenthesesStack(int s) {
+			maxSize = s;
+			stackArray = new String[maxSize];
+			top = -1;
+		}
+
+		public void push(String j) {
+			top += 1;
+			stackArray[top] = j;
+		}
+
+		public String pop() {
+			String temp = stackArray[top];
+			top -= 1;
+			return temp;
+		}
+
+		public String peek() {
+			return stackArray[top];
+		}
+
+		public boolean isEmpty() {
+			return (top == -1);
+		}
+
+		public boolean isFull() {
+			return (top == maxSize - 1);
+		}
+	}
+	
+	public boolean isValid(String s) {
+        
+        ParenthesesStack stack = new ParenthesesStack(s.length());
+
+        for (int i = 0; i < s.length(); i++) {
+            
+        	if (s.charAt(i) == '(' ||
+        		s.charAt(i) == '[' ||
+        		s.charAt(i) == '{') {
+                stack.push(String.valueOf(s.charAt(i)));
+            }
+            else {
+                if (!stack.isEmpty()) {
+                	if (s.charAt(i) == ')' && stack.peek().equals("(")) {
+                		stack.pop();
+                	}
+                	else if (s.charAt(i) == ']' && stack.peek().equals("[")) {
+                		stack.pop();
+                	}
+                	else if (s.charAt(i) == '}' && stack.peek().equals("{")) {
+                		stack.pop();
+                	}
+                	else {
+                		return false;
+                	}
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        if (stack.isEmpty()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+        
+    }
+	
+	public static void main(String[] args) {
+		
+		String inputString = "()[]{}";
+		
+		//String inputString = "()";
+		
+		//String inputString = "(]";
+		
+		//String inputString ="(])";
+		
+		ValidParentheses20 obj = new ValidParentheses20();
+		
+		System.out.println(obj.isValid(inputString));
+		
+		
+	}
+
+	
+
+}
