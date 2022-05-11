6130b7d586af5b67b30747a591a9be1d85433be1
Anshul Agrawal <anshul.agraw999@gmail.com>
2022-05-11 12:51:07 +0530

    314. Binary Tree Vertical Order Traversal

--- src/leetCodeProblems/BinaryTree/BinaryTreeVerticalOrderTraversal314.java
+++ src/leetCodeProblems/BinaryTree/BinaryTreeVerticalOrderTraversal314.java
@@ -26,39 +26,61 @@ public class BinaryTreeVerticalOrderTraversal314 {
         findMinMax(node.right, currentVerticalOrder+1);
     }
     
-    private void verticalOrderTraversalRecursion(TreeNode node, int currentVerticalOrder, int targetVerticalOrder) {
-        if (node == null) {
-            return;
+    private void bfsTraversal(TreeNode root) {
+    	Queue<TreeVNode> queue = new LinkedList<TreeVNode>();
+        
+        TreeVNode nodeV = new TreeVNode(root, 0);
+        queue.add(nodeV);
+        
+        TreeVNode currentVNode;
+        TreeNode currentNode; 
+        int currentNodeVOrder;
+        
+        TreeVNode currentLeftVNode;
+        TreeVNode currentRightVNode;
+        
+        while (!queue.isEmpty()) {
+            
+            currentVNode = queue.remove();
+            currentNode = currentVNode.node;
+            currentNodeVOrder = currentVNode.vOrder;
+            
+            if ( currentNode != null) {
+                
+                int currentNodeVOrderArrayListIndex = currentNodeVOrder-min;
+                result.get(currentNodeVOrderArrayListIndex).add(currentNode.val);
+                
+                if (currentNode.left != null) {
+                	currentLeftVNode = new TreeVNode(currentNode.left, currentNodeVOrder-1);
+                    queue.add(currentLeftVNode);
+                }
+                
+                if (currentNode.right != null) {
+                	currentRightVNode = new TreeVNode(currentNode.right, currentNodeVOrder+1);
+                    queue.add(currentRightVNode);
+                }
+            }
         }
         
-        if (currentVerticalOrder == targetVerticalOrder) {
-            int targetVerticalOrderIndex = targetVerticalOrder-min;
-            
-            System.out.println("targetVerticalOrder ->"+ targetVerticalOrderIndex);
-            List<Integer> temp = result.get(targetVerticalOrderIndex);
-            temp.add(node.val);
-        }
-            
-        verticalOrderTraversalRecursion(node.left, currentVerticalOrder-1, targetVerticalOrder);
-        verticalOrderTraversalRecursion(node.right, currentVerticalOrder+1, targetVerticalOrder);
-        
+        return;
     }
     
+    // Driver method
     public List<List<Integer>> verticalOrder(TreeNode root) {
+        
+        if (root == null) {
+            return new ArrayList<>();    
+        }
         
         findMinMax(root, 0);
         
         int numberOfVTraversalRows = max - min;
-            
-        System.out.println("numberOfVTraversalRows -> " + numberOfVTraversalRows);
+        
         for(int i = 0; i <= numberOfVTraversalRows; i++) {
             result.add(new ArrayList<Integer>());
         }
-        //obj.print();
         
-        for (int i=min; i <= max; i++) {
-            verticalOrderTraversalRecursion(root, 0, i);
-        }
+        bfsTraversal(root);
         
         return result;
         
@@ -80,16 +102,38 @@ public class BinaryTreeVerticalOrderTraversal314 {
         }
     }
     
+    static class TreeVNode {
+        TreeNode node;
+        int vOrder;
+        
+        TreeVNode(TreeNode node, int vOrder) {
+            this.node = node;
+            this.vOrder = vOrder;
+        }
+    }
+    
     public static void main(String[] args) {
         //System.out.println("Hello World!");
         
-        TreeNode root = new TreeNode(1);
+        /*TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
         root.right = new TreeNode(3);
         root.left.left = new TreeNode(4);
         root.left.right = new TreeNode(5);
         root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);*/
+        
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(0);
+        root.right.left = new TreeNode(1);
         root.right.right = new TreeNode(7);
+        
+        root.left.right.left = new TreeNode(5);
+        root.left.right.right = new TreeNode(2);
+        
         
         BinaryTreeVerticalOrderTraversal314 obj = new BinaryTreeVerticalOrderTraversal314();
         
