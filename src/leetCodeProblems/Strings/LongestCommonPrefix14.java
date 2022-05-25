9dcc30f976dd64f35d7363d1b70103bcf11a9f71
Anshul Agrawal <anshul.agraw999@gmail.com>
2022-05-25 13:55:24 +0530

    Longest Common Prefix

---
+++ src/leetCodeProblems/Strings/LongestCommonPrefix14.java
@@ -0,0 +1,26 @@
+package leetCodeProblems.Strings;
+
+public class LongestCommonPrefix14 {
+
+    public String longestCommonPrefix(String[] strs) {
+
+        String commonPrefix = "";
+
+        for (int i=0; i < strs.length; i++) {
+
+            String currentString = strs[i];
+
+            
+            //for (int j=0; j < )
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String[] strs = {"flower","flow","flight"};
+
+        LongestCommonPrefix14 obj = new LongestCommonPrefix14();
+        obj.longestCommonPrefix(strs);
+
+    }
+}
