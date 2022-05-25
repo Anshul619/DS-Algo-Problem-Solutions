2ca2fb590d5df3e94c2148e8afd05f41bbc1d621
Anshul Agrawal <anshul.agraw999@gmail.com>
2022-05-25 23:28:32 +0530

    14. Longest Common Prefix

--- src/leetCodeProblems/Strings/LongestCommonPrefix14.java
+++ src/leetCodeProblems/Strings/LongestCommonPrefix14.java
@@ -1,17 +1,46 @@
 package leetCodeProblems.Strings;
 
+/**
+ * LeetCode - https://leetcode.com/problems/longest-common-prefix/
+ */
+
 public class LongestCommonPrefix14 {
+
+    public String longestCommonPrefixUtil(String str1, String str2) {
+
+        String prefix = "";
+
+        for (int i=0, j=0; i < str1.length() && j < str2.length(); i++, j++) {
+
+            if (str1.charAt(i) == str2.charAt(j)) {
+                prefix += str1.charAt(i);
+            }
+            else {
+                break;
+            }
+        }
+
+        return prefix;
+
+    }
 
     public String longestCommonPrefix(String[] strs) {
 
         String commonPrefix = "";
 
-        for (int i=0; i < strs.length; i++) {
+        if (strs.length > 1) {
+            commonPrefix = longestCommonPrefixUtil(strs[0], strs[1]);
+        }
 
-            String currentString = strs[i];
+        System.out.println(commonPrefix);
 
+        for (int i=2; i < strs.length; i++) {
 
-            //for (int j=0; j < )
+            commonPrefix = longestCommonPrefixUtil(commonPrefix, strs[i]);
+
+            if (commonPrefix.isEmpty()) {
+                return "";
+            }
         }
 
         return commonPrefix;
@@ -19,10 +48,12 @@ public class LongestCommonPrefix14 {
 
     public static void main(String[] args) {
 
-        String[] strs = {"flower","flow","flight"};
+        //String[] strs = {"flower","flow","flight"};
+
+        String[] strs = {"cir","car"};
 
         LongestCommonPrefix14 obj = new LongestCommonPrefix14();
-        obj.longestCommonPrefix(strs);
+        System.out.println(obj.longestCommonPrefix(strs));
 
     }
 }
