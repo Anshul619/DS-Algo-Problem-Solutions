e8f15e7f41d9493054609d0d648e632dfbeda53d
Anshul Agrawal <anshul.agraw999@gmail.com>
2022-06-10 12:32:46 +0530

    136. Single Number

---
+++ src/leetCodeProblems/BitsManupulation/SingleNumber136.java
@@ -0,0 +1,30 @@
+package leetCodeProblems.BitsManupulation;
+
+/**
+ * LeetCode - https://leetcode.com/problems/single-number/
+ * Time Complexity - O(n)
+ * Space Complexity - O(1)
+ */
+public class SingleNumber136 {
+
+    public int singleNumber(int[] nums) {
+
+        int ansXOR = 0;
+
+        for (int i=0; i < nums.length; i++) {
+            //System.out.println("Before ->" + ansXOR);
+            ansXOR = ansXOR ^ nums[i];
+            //System.out.println("After ->" + ansXOR);
+        }
+
+        return ansXOR;
+    }
+
+    public static void main(String[] args) {
+
+        SingleNumber136 obj = new SingleNumber136();
+
+        int[] nums = {4,1,2,1,2};
+        System.out.println(obj.singleNumber(nums));
+    }
+}
