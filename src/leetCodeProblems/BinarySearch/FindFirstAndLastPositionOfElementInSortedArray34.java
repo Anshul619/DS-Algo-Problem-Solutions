1b0925b162dbb5762c51a4f8ec9350108647b78c
Anshul Agrawal <anshul.agraw999@gmail.com>
2022-06-18 22:29:49 +0530

    Find the Difference of Two Arrays

---
+++ src/leetCodeProblems/BinarySearch/FindFirstAndLastPositionOfElementInSortedArray34.java
@@ -0,0 +1,17 @@
+package leetCodeProblems.BinarySearch;
+
+public class FindFirstAndLastPositionOfElementInSortedArray34 {
+
+    public int[] searchRange(int[] nums, int target) {
+
+    }
+
+    public static void main(String[] args) {
+        FindFirstAndLastPositionOfElementInSortedArray34 obj = new FindFirstAndLastPositionOfElementInSortedArray34();
+
+        int[] nums = {5,7,7,8,8,10};
+        int target = 8;
+
+        obj.searchRange(nums, target);
+    }
+}
---
+++ src/leetCodeProblems/BinaryTree/CBTCountCompleteTreeNodes222.java
@@ -0,0 +1,24 @@
+package leetCodeProblems.BinaryTree;
+
+/**
+ * LeetCode - https://leetcode.com/problems/count-complete-tree-nodes/
+ *
+ * Approach
+ * -
+ */
+public class CBTCountCompleteTreeNodes222 {
+
+    static class TreeNode {
+        int val;
+        BinaryTreeSame100.TreeNode left;
+        BinaryTreeSame100.TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public int countNodes(TreeNode root) {
+
+    }
+}
