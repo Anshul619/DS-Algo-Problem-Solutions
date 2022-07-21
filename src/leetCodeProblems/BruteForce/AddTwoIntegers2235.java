774005aecb286abb144367de3a61d52437b36809
Anshul Agrawal <anshul.agraw999@gmail.com>
2022-07-21 23:12:23 +0530

    Add Two Integers

---
+++ src/leetCodeProblems/BruteForce/AddTwoIntegers2235.java
@@ -0,0 +1,11 @@
+package leetCodeProblems.BruteForce;
+
+/**
+ * LeetCode - https://leetcode.com/problems/add-two-integers/
+ */
+public class AddTwoIntegers2235 {
+
+    public int sum(int num1, int num2) {
+        return num1+num2;
+    }
+}
