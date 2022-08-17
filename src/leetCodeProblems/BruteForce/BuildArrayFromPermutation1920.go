d4454efe7f06f00a65cd460764380ecf95a43e81
Anshul Agrawal <anshul@touchnote.in>
2022-08-17 12:36:05 +0530

    1920. Build Array from Permutation

---
+++ src/leetCodeProblems/BruteForce/BuildArrayFromPermutation1920.go
@@ -0,0 +1,27 @@
+package main
+
+/**
+- LeetCode - https://leetcode.com/problems/build-array-from-permutation/
+- TimeComplexity - O(n)
+- SpaceComplexity - O(n)
+*/
+
+import "log"
+
+func buildArray(nums []int) []int {
+
+	output := make([]int, len(nums))
+
+	for i := 0; i < len(nums); i++ {
+		output[i] = nums[nums[i]]
+	}
+
+	return output
+}
+
+func main() {
+
+	input := []int{0, 2, 1, 5, 3, 4}
+
+	log.Println(buildArray(input)) //expected o/p = [0,1,2,4,5,3]
+}
