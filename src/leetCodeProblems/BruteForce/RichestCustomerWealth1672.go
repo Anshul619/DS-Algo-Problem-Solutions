9d878d6bd2be1ef9ad2e09bd3fdfaf9aa43c335a
Anshul Agrawal <anshul.agraw999@gmail.com>
2022-08-17 13:08:05 +0530

    1672. Richest Customer Wealth

---
+++ src/leetCodeProblems/BruteForce/RichestCustomerWealth1672.go
@@ -0,0 +1,35 @@
+package main
+
+/**
+- LeetCode - https://leetcode.com/problems/richest-customer-wealth/
+- TimeComplexity - O(n)
+- SpaceComplexity - O(n)
+*/
+import (
+	"log"
+	"math"
+)
+
+func maximumWealth(accounts [][]int) int {
+
+	var output, current float64
+
+	for _, value := range accounts {
+
+		current = 0
+		for _, value1 := range value {
+			current += float64(value1)
+		}
+
+		output = math.Max(output, current)
+	}
+
+	return int(output)
+}
+
+func main() {
+
+	input := [][]int{{10, 2, 3}, {3, 2, 5}}
+
+	log.Println(maximumWealth(input))
+}
