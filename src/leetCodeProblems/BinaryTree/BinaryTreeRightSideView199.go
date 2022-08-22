673dca6cfe4cf91322f1e2765d0e601d3d3177d2
Anshul Agrawal <anshul.agraw999@gmail.com>
2022-08-22 23:06:59 +0530

    199. Binary Tree Right Side View

---
+++ src/leetCodeProblems/BinaryTree/BinaryTreeRightSideView199.go
@@ -0,0 +1,50 @@
+package main
+
+/*
+- LeetCode - https://leetcode.com/problems/binary-tree-right-side-view/submissions/
+*/
+import "log"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func rightSideViewUtil(root *TreeNode, out *[]int, level int) {
+
+	if root == nil {
+		return
+	}
+
+	if len(*out) == level {
+		*out = append(*out, root.Val)
+	}
+
+	rightSideViewUtil(root.Right, out, level+1)
+	rightSideViewUtil(root.Left, out, level+1)
+}
+
+func rightSideView(root *TreeNode) []int {
+
+	var out []int
+
+	rightSideViewUtil(root, &out, 0)
+
+	return out
+}
+
+func main() {
+	root := new(TreeNode) //Returns pointer to TreeNode object
+	root.Val = 2
+	root.Left = new(TreeNode)
+	root.Left.Val = 1
+	root.Right = new(TreeNode)
+	root.Right.Val = 3
+	root.Right.Left = new(TreeNode)
+	root.Right.Left.Val = 0
+	root.Right.Right = new(TreeNode)
+	root.Right.Right.Val = 1
+
+	log.Println(rightSideView(root))
+}
