110498343fc64dabc572c965e008f9af8e8f7cb2
Anshul Agrawal <anshul.agraw999@gmail.com>
2022-08-23 23:15:40 +0530

    205. Isomorphic Strings

---
+++ src/leetCodeProblems/HashSearch/IsomorphicStrings205.go
@@ -0,0 +1,46 @@
+package main
+
+/*
+- LeetCode - https://leetcode.com/problems/isomorphic-strings/
+*/
+import "log"
+
+func isIsomorphic(s string, t string) bool {
+
+	dic := make(map[byte]byte)
+	dic1 := make(map[byte]byte)
+
+	for i, _ := range t {
+
+		if mapVal, ok := dic[t[i]]; ok && mapVal != s[i] {
+			return false
+		} else {
+			dic[t[i]] = s[i]
+		}
+
+		if mapVal, ok := dic1[s[i]]; ok && mapVal != t[i] {
+			return false
+		} else {
+			dic1[s[i]] = t[i]
+		}
+	}
+
+	return true
+}
+
+func main() {
+
+	s := "egg"
+	t := "add"
+
+	//s := "paper"
+	//t := "title"
+
+	//s := "foo"
+	//t := "bar"
+
+	//s := "badc"
+	//t := "baba"
+
+	log.Println(isIsomorphic(s, t))
+}
