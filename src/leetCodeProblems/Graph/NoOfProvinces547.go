7a3da28e3607cd6e0c98a9be70d6a6a8a836d800
Anshul Agrawal <anshul.agraw999@gmail.com>
2022-08-28 15:16:17 +0530

    547. Number of Provinces

---
+++ src/leetCodeProblems/Graph/NoOfProvinces547.go
@@ -0,0 +1,38 @@
+package main
+
+/*
+- LeetCode - https://leetcode.com/problems/number-of-provinces/submissions/
+*/
+import "log"
+
+func visitNeighbours(isConnected [][]int, visited []bool, vertex int) {
+
+	for i, v := range isConnected[vertex] {
+		if v == 1 && !visited[i] {
+			visited[i] = true
+			visitNeighbours(isConnected, visited, i)
+		}
+	}
+}
+
+func findCircleNum(isConnected [][]int) int {
+
+	vertices, out := len(isConnected), 0
+	visited := make([]bool, vertices)
+
+	for i := 0; i < vertices; i++ {
+		if !visited[i] {
+			visited[i] = true
+			out++
+			visitNeighbours(isConnected, visited, i)
+		}
+	}
+
+	return out
+}
+
+func main() {
+	//input := [][]int{{1, 1, 0}, {1, 1, 0}, {0, 0, 1}}
+	input := [][]int{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}
+	log.Println(findCircleNum(input))
+}
