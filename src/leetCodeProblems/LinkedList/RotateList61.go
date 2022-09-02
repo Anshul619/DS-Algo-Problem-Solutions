eb82a6eb0ca3c0bfc7df1f4ed9f1d79a83edfe45
Anshul Agrawal <anshul.agraw999@gmail.com>
2022-09-02 22:32:36 +0530

    61. Rotate List

---
+++ src/leetCodeProblems/LinkedList/RotateList61.go
@@ -0,0 +1,101 @@
+package main
+
+/*
+- LeetCode - https://leetcode.com/problems/rotate-list/
+*/
+import "log"
+
+type ListNode struct {
+	Next *ListNode
+	Val  int
+}
+
+func rotateRight(head *ListNode, k int) *ListNode {
+
+	fast := head
+	slow := head
+	total := 0
+
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	for fast.Next != nil {
+		fast = fast.Next
+		total++
+	}
+
+	total++
+
+	if k > total {
+		k = k % total
+	}
+
+	fast = head
+	for i := 0; i < k; i++ {
+		if fast.Next != nil {
+			fast = fast.Next
+		} else {
+			return head
+		}
+	}
+
+	for fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next
+	}
+
+	fast.Next = head
+	head = slow.Next
+	slow.Next = nil
+
+	return head
+}
+
+func print(node *ListNode) {
+	for node != nil {
+		log.Println(node.Val)
+		node = node.Next
+	}
+}
+func main() {
+
+	//head := ListNode{
+	//	Val: 10,
+	//	Next: &ListNode{
+	//		Val:  10,
+	//		Next: new(ListNode),
+	//	},
+	//}
+
+	// head := new(ListNode)
+	// head.Val = 0
+	// head.Next = new(ListNode)
+	// head.Next.Val = 1
+	// head.Next.Next = new(ListNode)
+	// head.Next.Next.Val = 2
+
+	// print(rotateRight(head, 4))
+
+	// head := new(ListNode)
+	// head.Val = 1
+	// head.Next = new(ListNode)
+	// head.Next.Val = 2
+	// head.Next.Next = new(ListNode)
+	// head.Next.Next.Val = 3
+	// head.Next.Next.Next = new(ListNode)
+	// head.Next.Next.Next.Val = 4
+	// head.Next.Next.Next.Next = new(ListNode)
+	// head.Next.Next.Next.Next.Val = 5
+
+	head := new(ListNode)
+	head.Val = 1
+	head.Next = new(ListNode)
+	head.Next.Val = 2
+
+	print(rotateRight(head, 5))
+
+	// //var head *ListNode
+
+	// print(rotateRight(head, 2))
+}
--- src/leetCodeProblems/LinkedList/SwapNodesInPairs24.go
+++ src/leetCodeProblems/LinkedList/SwapNodesInPairs24.go
@@ -5,12 +5,10 @@ package main
 - TimeCompelxity - O(n)
 */
 
-import "log"
-
-type ListNode struct {
-	Next *ListNode
-	Val  int
-}
+// type ListNode struct {
+// 	Next *ListNode
+// 	Val  int
+// }
 
 func swapPairs(head *ListNode) *ListNode {
 
@@ -45,44 +43,44 @@ func swapPairs(head *ListNode) *ListNode {
 	return head
 }
 
-func print(node *ListNode) {
-	for node != nil {
-		log.Println(node.Val)
-		node = node.Next
-	}
-}
-func main() {
+// func print(node *ListNode) {
+// 	for node != nil {
+// 		log.Println(node.Val)
+// 		node = node.Next
+// 	}
+// }
+// func main() {
 
-	//head := ListNode{
-	//	Val: 10,
-	//	Next: &ListNode{
-	//		Val:  10,
-	//		Next: new(ListNode),
-	//	},
-	//}
+// 	//head := ListNode{
+// 	//	Val: 10,
+// 	//	Next: &ListNode{
+// 	//		Val:  10,
+// 	//		Next: new(ListNode),
+// 	//	},
+// 	//}
 
-	//head := new(ListNode)
-	//head.Val = 1
-	//head.Next = new(ListNode)
-	//head.Next.Val = 2
-	//head.Next.Next = new(ListNode)
-	//head.Next.Next.Val = 3
+// 	//head := new(ListNode)
+// 	//head.Val = 1
+// 	//head.Next = new(ListNode)
+// 	//head.Next.Val = 2
+// 	//head.Next.Next = new(ListNode)
+// 	//head.Next.Next.Val = 3
 
-	head := new(ListNode)
-	head.Val = 1
-	head.Next = new(ListNode)
-	head.Next.Val = 2
-	head.Next.Next = new(ListNode)
-	head.Next.Next.Val = 3
-	head.Next.Next.Next = new(ListNode)
-	head.Next.Next.Next.Val = 4
-	head.Next.Next.Next.Next = new(ListNode)
-	head.Next.Next.Next.Next.Val = 5
+// 	head := new(ListNode)
+// 	head.Val = 1
+// 	head.Next = new(ListNode)
+// 	head.Next.Val = 2
+// 	head.Next.Next = new(ListNode)
+// 	head.Next.Next.Val = 3
+// 	head.Next.Next.Next = new(ListNode)
+// 	head.Next.Next.Next.Val = 4
+// 	head.Next.Next.Next.Next = new(ListNode)
+// 	head.Next.Next.Next.Next.Val = 5
 
-	// head := new(ListNode)
-	// head.Val = 1
+// 	// head := new(ListNode)
+// 	// head.Val = 1
 
-	//var head *ListNode
+// 	//var head *ListNode
 
-	print(swapPairs(head))
-}
+// 	print(swapPairs(head))
+// }
