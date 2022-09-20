17e67d252bead352c8a3ed71b26d2c16d4834257
Anshul Agrawal <anshul.agraw999@gmail.com>
2022-09-20 11:00:44 +0530

    696. Count Binary Substrings

---
+++ src/leetCodeProblems/HashSearch/CountBinarySubStrings696.go
@@ -0,0 +1,63 @@
+package main
+
+/*
+- LeetCode - https://leetcode.com/problems/count-binary-substrings/
+*/
+import "log"
+
+func countBinarySubstrings(s string) int {
+
+	out := 0
+	zeroCount := 0
+	oneCount := 0
+
+	i := 0
+
+	for i < len(s) {
+
+		oneCount, zeroCount = 0, 0
+		if s[i] == '0' {
+
+			for i < len(s) && s[i] == '0' {
+				zeroCount++
+				i++
+			}
+
+			j := i
+
+			for j < len(s) && s[j] == '1' {
+				oneCount++
+				j++
+			}
+
+		} else {
+
+			for i < len(s) && s[i] == '1' {
+				oneCount++
+				i++
+			}
+
+			j := i
+
+			for j < len(s) && s[j] == '0' {
+				zeroCount++
+				j++
+			}
+		}
+
+		if oneCount < zeroCount {
+			out += oneCount
+		} else {
+			out += zeroCount
+		}
+	}
+
+	return out
+}
+
+func main() {
+
+	//s := "00110011"
+	s := "00100"
+	log.Println(countBinarySubstrings(s))
+}
