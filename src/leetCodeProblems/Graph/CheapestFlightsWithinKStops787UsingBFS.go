c2795f638012f54fff9be605501955e8a8e28808
Anshul Agrawal <anshul.agraw999@gmail.com>
2022-10-26 13:27:54 +0530

    200. Number of Islands

--- src/leetCodeProblems/Graph/CheapestFlightsWithinKStops787UsingBFS.go
+++ src/leetCodeProblems/Graph/CheapestFlightsWithinKStops787UsingBFS.go
@@ -4,7 +4,6 @@ package main
 - LeetCode - https://leetcode.com/problems/cheapest-flights-within-k-stops
 */
 import (
-	"log"
 	"math"
 )
 
@@ -106,19 +105,19 @@ func findCheapestPrice(n int, flights [][]int, src int, dst int, k int) int {
 	return minCost
 }
 
-func main() {
-	//input := [][]int{{0, 1, 100}, {1, 2, 100}, {2, 0, 100}, {1, 3, 600}, {2, 3, 200}}
-	//log.Println(findCheapestPrice(4, input, 0, 3, 1))
+// func main() {
+// 	//input := [][]int{{0, 1, 100}, {1, 2, 100}, {2, 0, 100}, {1, 3, 600}, {2, 3, 200}}
+// 	//log.Println(findCheapestPrice(4, input, 0, 3, 1))
 
-	// input := [][]int{{0, 1, 100}, {1, 2, 100}}
-	// log.Println(findCheapestPrice(3, input, 0, 2, 1))
+// 	// input := [][]int{{0, 1, 100}, {1, 2, 100}}
+// 	// log.Println(findCheapestPrice(3, input, 0, 2, 1))
 
-	//input := [][]int{{0, 1, 100}, {1, 2, 100}, {0, 2, 500}}
-	//log.Println(findCheapestPrice(3, input, 0, 2, 0))
+// 	//input := [][]int{{0, 1, 100}, {1, 2, 100}, {0, 2, 500}}
+// 	//log.Println(findCheapestPrice(3, input, 0, 2, 0))
 
-	//input := [][]int{{3, 4, 4}, {2, 5, 6}, {4, 7, 10}, {9, 6, 5}, {7, 4, 4}, {6, 2, 10}, {6, 8, 6}, {7, 9, 4}, {1, 5, 4}, {1, 0, 4}, {9, 7, 3}, {7, 0, 5}, {6, 5, 8}, {1, 7, 6}, {4, 0, 9}, {5, 9, 1}, {8, 7, 3}, {1, 2, 6}, {4, 1, 5}, {5, 2, 4}, {1, 9, 1}, {7, 8, 10}, {0, 4, 2}, {7, 2, 8}}
-	//log.Println(findCheapestPrice(10, input, 6, 0, 7))
+// 	//input := [][]int{{3, 4, 4}, {2, 5, 6}, {4, 7, 10}, {9, 6, 5}, {7, 4, 4}, {6, 2, 10}, {6, 8, 6}, {7, 9, 4}, {1, 5, 4}, {1, 0, 4}, {9, 7, 3}, {7, 0, 5}, {6, 5, 8}, {1, 7, 6}, {4, 0, 9}, {5, 9, 1}, {8, 7, 3}, {1, 2, 6}, {4, 1, 5}, {5, 2, 4}, {1, 9, 1}, {7, 8, 10}, {0, 4, 2}, {7, 2, 8}}
+// 	//log.Println(findCheapestPrice(10, input, 6, 0, 7))
 
-	input := [][]int{{1, 2, 10}, {2, 0, 7}, {1, 3, 8}, {4, 0, 10}, {3, 4, 2}, {4, 2, 10}, {0, 3, 3}, {3, 1, 6}, {2, 4, 5}}
-	log.Println(findCheapestPrice(5, input, 0, 4, 1))
-}
+// 	input := [][]int{{1, 2, 10}, {2, 0, 7}, {1, 3, 8}, {4, 0, 10}, {3, 4, 2}, {4, 2, 10}, {0, 3, 3}, {3, 1, 6}, {2, 4, 5}}
+// 	log.Println(findCheapestPrice(5, input, 0, 4, 1))
+// }
---
+++ src/leetCodeProblems/Graph/NoOfIslands200.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bytes"
+	"log"
+)
+
+func dfs(grid [][]byte, x int, y int) {
+
+	if x < 0 || x >= len(grid) || y < 0 || y >= len(grid[0]) || !bytes.Equal([]byte{grid[x][y]}, []byte{'1'}) {
+		return
+	}
+
+	grid[x][y] = 0
+
+	dfs(grid, x-1, y)
+	dfs(grid, x+1, y)
+	dfs(grid, x, y-1)
+	dfs(grid, x, y+1)
+}
+
+func numIslands(grid [][]byte) int {
+
+	numberOfIslands := 0
+	for i := range grid {
+		for j := range grid[i] {
+			if bytes.Equal([]byte{grid[i][j]}, []byte{'1'}) {
+				numberOfIslands++
+				dfs(grid, i, j)
+			}
+		}
+	}
+
+	return numberOfIslands
+}
+
+func main() {
+
+	// grid := [][]byte{{'1', '1', '1', '1', '0'}, {'1', '1', '0', '1', '0'},
+	// 	{'1', '1', '0', '0', '0'},
+	// 	{'0', '0', '0', '0', '0'}}
+
+	// log.Println(numIslands(grid))
+
+	grid := [][]byte{{'1', '1', '0', '0', '0'},
+		{'1', '1', '0', '0', '0'},
+		{'0', '0', '1', '0', '0'},
+		{'0', '0', '0', '1', '1'}}
+
+	log.Println(numIslands(grid))
+}
