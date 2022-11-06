d54c2e1f8cf69d24f163e815035e1502c42f5d0b
Anshul Agrawal <anshul.agraw999@gmail.com>
2022-11-06 14:28:36 +0530

    Single Number

---
+++ src/leetCodeProblems/BitsManupulation/SingleNumber136.go
@@ -0,0 +1,20 @@
+package main
+
+import "log"
+
+func singleNumber(nums []int) int {
+	ansXOR := 0
+
+	for _, v := range nums {
+		ansXOR ^= v
+	}
+
+	return ansXOR
+
+}
+
+func main() {
+	nums := []int{4, 1, 2, 1, 2}
+
+	log.Println(singleNumber(nums))
+}
