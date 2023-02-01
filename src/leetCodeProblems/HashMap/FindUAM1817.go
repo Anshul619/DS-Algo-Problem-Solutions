d977672e315fce5c8785224ff5a04e79171fa350
Anshul Agrawal <anshul.agraw999@gmail.com>
2023-02-01 10:50:40 +0530

    1817. Finding the Users Active Minutes

---
+++ src/leetCodeProblems/HashMap/FindUAM1817.go
@@ -0,0 +1,40 @@
+package main
+
+/*
+- LeetCode - https://leetcode.com/problems/finding-the-users-active-minutes/description/
+- Time - O(n)
+- Space - O(n)
+*/
+
+import "log"
+
+func findingUsersActiveMinutes(logs [][]int, k int) []int {
+
+	m := make(map[int]map[int]bool)
+	out := make([]int, k)
+
+	for _, v := range logs {
+
+		if _, ok := m[v[0]]; ok {
+
+			if _, ok1 := m[v[0]][v[1]]; !ok1 {
+				m[v[0]][v[1]] = true
+			}
+		} else {
+			m[v[0]] = make(map[int]bool)
+			m[v[0]][v[1]] = true
+		}
+	}
+
+	for _, v := range m {
+		out[len(v)-1]++
+	}
+
+	return out
+}
+
+func main() {
+	logs := [][]int{{0, 5}, {1, 2}, {0, 2}, {0, 5}, {1, 3}}
+
+	log.Println(findingUsersActiveMinutes(logs, 5))
+}
--- src/leetCodeProblems/HashMap/MaxTwinSumLinkedList2130UsingHashMap.go
+++ src/leetCodeProblems/HashMap/MaxTwinSumLinkedList2130UsingHashMap.go
@@ -3,7 +3,6 @@ package main
 /**
 - LeetCode - https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/submissions/
 */
-import "log"
 
 type ListNode struct {
 	Val  int
@@ -38,17 +37,17 @@ func pairSumUsingHashMap(head *ListNode) int {
 	return maxSum
 }
 
-func main() {
+// func main() {
 
-	head := new(ListNode)
-	head.Val = 5
-	head.Next = new(ListNode)
-	head.Next.Val = 4
-	head.Next.Next = new(ListNode)
-	head.Next.Next.Val = 2
-	head.Next.Next.Next = new(ListNode)
-	head.Next.Next.Next.Val = 1
+// 	head := new(ListNode)
+// 	head.Val = 5
+// 	head.Next = new(ListNode)
+// 	head.Next.Val = 4
+// 	head.Next.Next = new(ListNode)
+// 	head.Next.Next.Val = 2
+// 	head.Next.Next.Next = new(ListNode)
+// 	head.Next.Next.Next.Val = 1
 
-	log.Println(pairSumUsingHashMap(head))
+// 	log.Println(pairSumUsingHashMap(head))
 
-}
+// }
