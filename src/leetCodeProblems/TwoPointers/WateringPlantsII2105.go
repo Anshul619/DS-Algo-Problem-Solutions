c6451e7e6ba57c75c08effed705338f43ca0b804
Anshul Agrawal <anshul.agraw999@gmail.com>
2023-02-17 23:32:23 +0530

    2105. Watering Plants II

---
+++ src/leetCodeProblems/TwoPointers/WateringPlantsII2105.go
@@ -0,0 +1,63 @@
+package main
+
+/*
+- LeetCode - https://leetcode.com/submissions/detail/899901477/
+- Time - O(n)
+- Space - O(1)
+*/
+import "log"
+
+func getMax(i, j int) int {
+	if i > j {
+		return i
+	}
+
+	return j
+}
+
+func minimumRefill(plants []int, capacityA int, capacityB int) int {
+
+	out := 0
+	currentA, currentB := 0, len(plants)-1
+	bucketA, bucketB := capacityA, capacityB
+
+	for currentA <= currentB {
+
+		if currentA == currentB {
+			if getMax(bucketA, bucketB) < plants[currentA] {
+				out++
+			}
+			break
+		} else {
+
+			if bucketA < plants[currentA] {
+				out++
+				bucketA = capacityA
+			}
+
+			if bucketB < plants[currentB] {
+				out++
+				bucketB = capacityB
+			}
+
+			bucketA -= plants[currentA]
+			bucketB -= plants[currentB]
+
+			currentA++
+			currentB--
+
+		}
+	}
+
+	return out
+}
+
+func main() {
+	//plants := []int{2, 2, 3, 3}
+
+	//log.Println(minimumRefill(plants, 5, 5))
+	//log.Println(minimumRefill(plants, 3, 4))
+
+	plants := []int{5}
+	log.Println(minimumRefill(plants, 10, 8))
+}
