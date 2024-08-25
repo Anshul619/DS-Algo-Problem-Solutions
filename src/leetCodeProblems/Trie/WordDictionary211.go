e60425ecf9094e4711c63b1787641501b715f740
Anshul Agrawal <anshul.agraw999@gmail.com>
2024-08-25 13:58:23 +0530

    Commit

---
+++ src/leetCodeProblems/Trie/WordDictionary211.go
@@ -0,0 +1,70 @@
+package main
+
+/*
+- Leetcode - https://leetcode.com/problems/design-add-and-search-words-data-structure/description/
+- Time - O(n)
+- Space - O(n)
+*/
+type WordNode struct {
+	children    [26]*WordNode
+	isEndOfWord bool
+}
+
+type WordDictionary struct {
+	root *WordNode
+}
+
+func Constructor1() WordDictionary {
+	w := WordDictionary{}
+	w.root = new(WordNode)
+	return w
+}
+
+func (this *WordDictionary) AddWord(word string) {
+	w := this.root
+
+	for _, v := range word {
+		v -= 'a'
+
+		if w.children[v] == nil {
+			w.children[v] = new(WordNode)
+		}
+
+		w = w.children[v]
+	}
+
+	w.isEndOfWord = true
+}
+
+func searchUtil(word string, root *WordNode) bool {
+	if root == nil {
+		return false
+	}
+
+	w := root
+
+	for i, v := range word {
+		if v == '.' {
+			for j := 0; j < 26; j++ {
+				if w.children[j] != nil {
+					if searchUtil(word[i+1:], w.children[j]) {
+						return true
+					}
+				}
+			}
+			return false
+		}
+
+		v -= 'a'
+
+		if w.children[v] == nil {
+			return false
+		}
+		w = w.children[v]
+	}
+	return w != nil && w.isEndOfWord
+}
+
+func (this *WordDictionary) Search(word string) bool {
+	return searchUtil(word, this.root)
+}
