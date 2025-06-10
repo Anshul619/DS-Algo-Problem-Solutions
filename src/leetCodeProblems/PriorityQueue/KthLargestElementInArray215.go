cde13b6892909afcc5d89dbcd57fcb25493c9862
Anshul Agrawal <anshul.agraw999@gmail.com>
2025-06-10 21:46:14 +0530

    Commit

--- src/leetCodeProblems/PriorityQueue/KthLargestElementInArray215.go
+++ src/leetCodeProblems/PriorityQueue/KthLargestElementInArray215.go
@@ -9,18 +9,46 @@ import (
 - Time - O(N*LogK)
 - Space - O(k)
 */
+type minHeap1 []int
+
+func (h minHeap1) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+
+func (h minHeap1) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h minHeap1) Len() int {
+	return len(h)
+}
+
+func (h *minHeap1) Pop() interface{} {
+	t := (*h)[len(*h)-1]
+	*h = (*h)[:len(*h)-1]
+	return t
+}
+
+func (h *minHeap1) Push(t interface{}) {
+	*h = append(*h, t.(int))
+}
+
+func (h minHeap1) Peek() int {
+	return h[0]
+}
+
 func findKthLargest(nums []int, k int) int {
-	pq := new(elementsQueue)
+	pq := new(minHeap1)
+	heap.Init(pq)
 
 	for _, v := range nums {
-		if pq.Len() == k {
-			if v <= pq.Peek().(int) {
-				continue
-			}
-
+		if pq.Len() < k {
+			heap.Push(pq, v)
+		} else if pq.Peek() < v {
 			heap.Pop(pq)
+			heap.Push(pq, v)
 		}
-		heap.Push(pq, v)
 	}
-	return pq.Peek().(int)
+
+	return heap.Pop(pq).(int)
 }
