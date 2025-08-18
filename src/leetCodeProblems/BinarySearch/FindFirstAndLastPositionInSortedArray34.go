2128218483a9b18a2fe2fcc9d9a69be690b8f0e7
Anshul Agrawal <anshul.agraw999@gmail.com>
2025-08-18 19:15:47 +0530

    Commit

--- src/leetCodeProblems/BinarySearch/FindFirstAndLastPositionInSortedArray34.go
+++ src/leetCodeProblems/BinarySearch/FindFirstAndLastPositionInSortedArray34.go
@@ -5,58 +5,48 @@ package main
 - Time - O(log n)
 - Space - O(1)
 */
-func findLowerRange(nums []int, target int) int {
-	out := -1
-
-	start := 0
-	end := len(nums) - 1
-
-	for start <= end {
-
-		mid := start + (end-start)/2
-
-		if nums[mid] == target {
-			out = mid
-		}
-
-		if nums[mid] >= target {
-			end = mid - 1
-		} else {
-			start = mid + 1
-		}
-	}
-	return out
-}
-
-func findUpperRange(nums []int, target int) int {
-	out := -1
-
-	start := 0
-	end := len(nums) - 1
-
-	for start <= end {
-
-		mid := start + (end-start)/2
-
-		if nums[mid] == target {
-			out = mid
-		}
-
-		if nums[mid] <= target {
-			start = mid + 1
-		} else {
-			end = mid - 1
-		}
-	}
-	return out
-}
-
 func searchRange(nums []int, target int) []int {
-	if len(nums) == 0 {
-		return []int{-1, -1}
+	out := []int{-1, -1}
+	start := 0
+	end := len(nums) - 1
+
+	// Find the leftmost index
+	for start <= end {
+		mid := start + (end-start)/2
+
+		if nums[mid] == target {
+			if mid > 0 && nums[mid-1] == target {
+				end = mid - 1
+			} else {
+				out[0] = mid
+				break
+			}
+		} else if nums[mid] < target {
+			start = mid + 1
+		} else {
+			end = mid - 1
+		}
 	}
-	out := []int{}
-	out = append(out, findLowerRange(nums, target))
-	out = append(out, findUpperRange(nums, target))
+
+	// Find the rightmost index
+	start = 0
+	end = len(nums) - 1
+
+	for start <= end {
+		mid := start + (end-start)/2
+
+		if nums[mid] == target {
+			if mid < len(nums)-1 && nums[mid+1] == target {
+				start = mid + 1
+			} else {
+				out[1] = mid
+				break
+			}
+		} else if nums[mid] < target {
+			start = mid + 1
+		} else {
+			end = mid - 1
+		}
+	}
 	return out
 }
