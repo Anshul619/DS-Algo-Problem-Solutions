fcb86be9d6ae3a4bc9a274f55f939762e11db7d0
Anshul Agrawal <anshul.agraw999@gmail.com>
2025-08-28 12:52:17 +0530

    Commit

--- src/leetCodeProblems/TwoPointers/ValidPalindrome125.go
+++ src/leetCodeProblems/TwoPointers/ValidPalindrome125.go
@@ -20,62 +20,29 @@ func isAlphanumeric(b byte) bool {
 }
 
 func isPalindrome(s string) bool {
-	left, right := 0, len(s)-1
+	left := 0
+	right := len(s) - 1
+
+	s = strings.ToLower(s)
 
 	for left < right {
-
+		if left == right {
+			return false
+		}
 		if !isAlphanumeric(s[left]) {
 			left++
 			continue
 		}
-
 		if !isAlphanumeric(s[right]) {
 			right--
 			continue
 		}
 
-		if !strings.EqualFold(string(s[left]), string(s[right])) {
+		if s[left] != s[right] {
 			return false
 		}
 		left++
 		right--
 	}
-
 	return true
 }
-
-// Alternative solution
-func isPalindrome2(s string) bool {
-	s = strings.ToLower(s)
-	f, l := 0, len(s)-1
-
-	for f <= l {
-		switch {
-
-		case !unicode.IsLetter(rune(s[f])) && !unicode.IsDigit(rune(s[f])):
-			f++
-		case !unicode.IsLetter(rune(s[l])) && !unicode.IsDigit(rune(s[l])):
-			l--
-		default:
-			if s[f] != s[l] {
-				return false
-			} else {
-				f++
-				l--
-			}
-		}
-	}
-
-	return true
-}
-
-// func main() {
-// 	//plants := []int{2, 2, 3, 3}
-
-// 	//log.Println(minimumRefill(plants, 5, 5))
-// 	//log.Println(minimumRefill(plants, 3, 4))
-
-// 	//plants := []int{5}
-// 	log.Println(isPalindrome("A man, a plan, a canal: Panama"))
-// 	log.Println(isPalindrome("race a car"))
-// }
