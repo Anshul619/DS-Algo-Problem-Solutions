404ca932a80a4cd25742573a127803e8d95339f3
Anshul Agrawal <anshul.agraw999@gmail.com>
2025-09-29 20:04:55 +0530

    Commit

--- src/leetCodeProblems/Stacks/EvaluateReversePolishNumber150.go
+++ src/leetCodeProblems/Stacks/EvaluateReversePolishNumber150.go
@@ -1,61 +1,47 @@
 package main
 
+import "strconv"
+
 /*
 - LeetCode - https://leetcode.com/problems/evaluate-reverse-polish-notation
 - Time - O(n)
 - Space - O(n)
 */
-import (
-	"strconv"
-)
+type stackRPN []int
 
-type stackRPN []string
-
-func (s *stackRPN) push(str string) {
-	*s = append(*s, str)
-}
-
-func (s *stackRPN) pop() string {
-	str := (*s)[len(*s)-1]
+func (s *stackRPN) pop() int {
+	t := (*s)[len(*s)-1]
 	*s = (*s)[:len(*s)-1]
-	return str
+	return t
 }
 
-func (s *stackRPN) pop2Times() (string, string) {
-	str := (*s)[len(*s)-2:]
-	*s = (*s)[:len(*s)-2]
-	return str[0], str[1]
+func (s *stackRPN) push(t int) {
+	*s = append(*s, t)
 }
 
 func evalRPN(tokens []string) int {
-
-	type fn func(int, int) int
-
 	s := new(stackRPN)
 
-	m := map[string]fn{
-		"+": func(a int, b int) int { return a + b },
-		"-": func(a int, b int) int { return a - b },
-		"*": func(a int, b int) int { return a * b },
-		"/": func(a int, b int) int { return a / b },
-	}
-
 	for _, v := range tokens {
-
-		if fn, ok := m[v]; ok {
-
-			n1, n2 := s.pop2Times()
-
-			n3, _ := strconv.Atoi(n1)
-			n4, _ := strconv.Atoi(n2)
-
-			v = strconv.Itoa(fn(n3, n4))
+		switch v {
+		case "+", "-", "/", "*":
+			n1 := s.pop()
+			n2 := s.pop()
+			switch v {
+			case "+":
+				s.push(n2 + n1)
+			case "-":
+				s.push(n2 - n1)
+			case "/":
+				s.push(n2 / n1)
+			default:
+				s.push(n2 * n1)
+			}
+		default:
+			i, _ := strconv.Atoi(v)
+			s.push(i)
 
 		}
-		s.push(v)
 	}
-
-	out, _ := strconv.Atoi(s.pop())
-
-	return out
+	return s.pop()
 }
